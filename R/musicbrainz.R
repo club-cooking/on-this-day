2856114fad74be3a7e0aecc07db242fe7e428b94
ewen_ <ewenhenderson@gmail.com>
2022-01-22 18:11:11 +0000

    first

---
+++ R/musicbrainz.R
@@ -0,0 +1,52 @@
+library(httr2)
+library(glue)
+library(dplyr)
+library(purrr)
+
+url_root <- "https://musicbrainz.org/ws/2/"
+entity_type <- "release"
+release_date <- "2012-01-27"
+
+# base request
+request <- request(url_root) %>% 
+  req_url_path_append(entity_type) %>% 
+  req_headers(
+    Accept = "application/json"
+  ) 
+
+# first search
+search <- request %>% 
+  req_url_query(
+    query = glue("date:{release_date}"), 
+    limit = 100
+    ) %>% 
+  req_perform() %>% 
+  resp_body_json() 
+
+# get no. records/pages
+n_results <- search$count
+n_pages <- ceiling(n_results / 100)
+
+# query full results set
+result <- map_dfr(seq_along(1:n_pages), function(x){
+  
+  n <- x - 1
+  offset = 100 * n
+  
+  result <- request %>% 
+    req_url_query(
+      query = glue("date:{release_date}"), 
+      limit = 100, offset = offset
+    ) %>% 
+    req_throttle(rate = 30 / 60) %>% 
+    req_perform() %>% 
+    resp_body_json() 
+  
+  result$releases %>% 
+    purrr::transpose() %>% 
+    tidyr::unchop() %>% 
+    as_tibble()
+})
+
+# exact release date matches
+result_exact <- dplyr::filter(result, date == release_date)
